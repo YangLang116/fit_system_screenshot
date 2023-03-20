50fc61232dc73f8a66f7f8a4bfd29ca6e1215a7b
YangLang <yanglang116@gmail.com>
2023-03-20 12:36:14 +0800

    添加系统判断逻辑

--- lib/fit_system_screenshot.dart
+++ lib/fit_system_screenshot.dart
@@ -1,3 +1,4 @@
+import 'dart:io';
 import 'dart:ui';
 
 import 'package:flutter/cupertino.dart';
@@ -6,12 +7,13 @@ import 'package:flutter/services.dart';
 const String _METHOD_CHANNEL_NAME = 'fit.system.screenshot';
 
 class _FitSystemScreenshot {
-  late MethodChannel _methodChannel;
+  MethodChannel? _methodChannel;
   ScrollController? _scrollController;
 
   _FitSystemScreenshot() {
+    if (!Platform.isAndroid) return;
     this._methodChannel = MethodChannel(_METHOD_CHANNEL_NAME);
-    this._methodChannel.setMethodCallHandler((call) {
+    this._methodChannel!.setMethodCallHandler((call) {
       if (_scrollController == null) return Future.value(false);
       String methodName = call.method;
       if (methodName == 'scroll') {
@@ -25,20 +27,23 @@ class _FitSystemScreenshot {
   }
 
   void attach(ScrollController scrollController) {
+    if (!Platform.isAndroid) return;
     this._scrollController = scrollController;
   }
 
   void updateScrollArea(Rect scrollArea) {
+    if (!Platform.isAndroid) return;
     double radio = window.devicePixelRatio;
     int top = (scrollArea.top * radio).toInt();
     int left = (scrollArea.left * radio).toInt();
     int width = (scrollArea.width * radio).toInt();
     int height = (scrollArea.height * radio).toInt();
-    _methodChannel.invokeMethod('updateScrollArea',
+    _methodChannel?.invokeMethod('updateScrollArea',
         {'top': top, 'left': left, 'width': width, 'height': height});
   }
 
   void detach() {
+    if (!Platform.isAndroid) return;
     this._scrollController = null;
   }
 }
